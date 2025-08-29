50015e104a4b0a647e5fbbd31ee5670d4c426876
idtcipp <jason.tems+cipp@idt-ltd.co.uk>
2025-08-29 16:56:32 +0100

    Update Windows.ps1

--- OSDCloud/Universal/Windows.ps1
+++ OSDCloud/Universal/Windows.ps1
@@ -1,12 +1,7 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
-
 Start-OSDCloud -OSLanguage en-GB -OSLicense Retail
-
 #Restart from WinPE
-
 Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
-
 Start-Sleep -Seconds 20
-
 wpeutil reboot
